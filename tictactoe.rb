4bf420de9d195c01837d751a8b861ccfb386af7d
mdmccoy <mdmccoy@gmail.com>
2018-01-09 21:11:36 -0500

    Win condition and restart loop need work.

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,5 +1,6 @@
 #we're going to make checkers!
 def draw_board(board)
+  puts "\e[H\e[2J"  #I have no idea what this is doing. Need to look up.
   p spacer = ["_","0","1","2"]
   i = 0
   board.each do |b|
@@ -27,7 +28,13 @@ def get_move
 end
 
 def check_win(board)
-
+  win = false
+  board.each do |a|
+    if a[0] == 'X' && a[1] == 'X' && a[2] =='X'
+      win = true
+    end
+  end
+  win
 end
 
 
@@ -38,10 +45,15 @@ board = [
   ["_","_","_"]]
 
 while continue.downcase == 'y' do
-  puts "\e[H\e[2J"
   draw_board(board)
   move = get_move
   make_move(board,move)
-  puts "Continue? (y/n)"
-  continue = gets.chomp.downcase
+  if check_win(board)
+    puts "You won the game!"
+    puts "Play again? (y/n)"
+    continue = gets.chomp.downcase
+    if continue != 'y'
+      break
+    end
+  end
 end
