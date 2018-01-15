06f3976f3b5d75f395c28574c684434f0592277f
mdmccoy <mdmccoy@gmail.com>
2018-01-15 09:00:38 -0500

    I started turning it into a class...

--- tictactoe.rb
+++ tictactoe.rb
@@ -19,7 +19,7 @@ class TicTacToe
     [" "," "," "]]
   end
 
-  def draw_board(board)
+  def draw_board()
     #draws the current board to the screen
 
     #clears screen. I have no idea what this is doing. Need to look up.
@@ -28,13 +28,13 @@ class TicTacToe
        0   1   2
 
          |   |
-    0  #{board[0][0]} | #{board[0][1]} | #{board[0][2]}
+    0  #{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]}
       ___|___|___
          |   |
-    1  #{board[1][0]} | #{board[1][1]} | #{board[1][2]}
+    1  #{@board[1][0]} | #{@board[1][1]} | #{@board[1][2]}
       ___|___|___
          |   |
-    2  #{board[2][0]} | #{board[2][1]} | #{board[2][2]}
+    2  #{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]}
          |   |
 
 
@@ -56,7 +56,7 @@ class TicTacToe
     player
   end
 
-  def make_move(board,move,player)
+  def make_move(move,player)
     #takes a board and the move in a two element array, puts the move on the board
     #returns a board
 
@@ -64,13 +64,13 @@ class TicTacToe
     player == 1 ? marker = "X" : marker = "O"
 
     #place the marker on the board
-    board[move[0]][move[1]] = marker
+    @board[move[0]][move[1]] = marker
   end
 
-  def ai_move(board)
+  def ai_move()
     #takes a board and makes moves for the AI by picking random moves until one is valid
     ai_move = [Random.rand(3),Random.rand(3)]
-    until check_move?(board, ai_move)
+    until check_move?(@board, ai_move)
       ai_move = [Random.rand(3),Random.rand(3)]
     end
     ai_move
@@ -92,20 +92,20 @@ class TicTacToe
     move = move.map {|e| e.to_i}
   end
 
-  def check_move?(board,move)
+  def check_move?(move)
     #checks to see if the move is valid or not, takes a board (3x3 array) and a two element array
     #this gets called by ai_move so don't change this!
-    if board[move[0]][move[1]] == " "
+    if @board[move[0]][move[1]] == " "
       true
     else
       false
     end
   end
 
-  def check_stalemate?(board,player)
+  def check_stalemate?(player)
     #checks to see if we have played to a stalemate
     stalemate = true
-    board.each do |a|
+    @board.each do |a|
       if a.include?(" ")
         stalemate = false
       end
@@ -113,7 +113,7 @@ class TicTacToe
     stalemate
   end
 
-  def check_win?(board,player)
+  def check_win?(player)
     #check the player and set the marker for comparisons
     player == 1 ? marker = "X" : marker = "O"
 
@@ -121,18 +121,18 @@ class TicTacToe
     win = false
 
     #check the rows
-    board.each do |a|
+    @board.each do |a|
       win = true if a[0] == marker && a[1] == marker && a[2] ==marker
     end
 
     #check the columns, there has to be a better way to do this.
-    win = true if board[0][0] == marker && board[1][0] == marker && board[2][0] == marker
-    win = true if board[0][1] == marker && board[1][1] == marker && board[2][1] == marker
-    win = true if board[0][2] == marker && board[1][2] == marker && board[2][2] == marker
+    win = true if @board[0][0] == marker && @board[1][0] == marker && @board[2][0] == marker
+    win = true if @board[0][1] == marker && @board[1][1] == marker && @board[2][1] == marker
+    win = true if @board[0][2] == marker && @board[1][2] == marker && @board[2][2] == marker
 
     #check the two diag directions
-    win = true if board[0][0] == marker && board[1][1] == marker && board[2][2] == marker
-    win = true if board[0][2] == marker && board[1][1] == marker && board[2][0] == marker
+    win = true if @board[0][0] == marker && @board[1][1] == marker && @board[2][2] == marker
+    win = true if @board[0][2] == marker && @board[1][1] == marker && @board[2][0] == marker
 
     #return win
     win
@@ -141,7 +141,7 @@ class TicTacToe
   def run_game
     #initialize our variables
     continue = 'Y'
-    board = new_board
+    #board = new_board
     player = 2
     #num_players = 0
 
@@ -156,7 +156,7 @@ class TicTacToe
     while continue.downcase == 'y' do
 
 
-      draw_board(board)
+      draw_board()
 
 
       player = swap_player(player)
@@ -173,24 +173,24 @@ class TicTacToe
 
           #elsif we are player 2 in a 1 player game, we are the AI
         elsif (player == 2 && @num_players == 1)
-          ai_move(board)
+          ai_move
         end
 
-        move_good = check_move?(board,move) # maybe move this entire loop to get_move?
+        move_good = check_move?(move) # maybe move this entire loop to get_move?
         puts "Invalid move, please try again!" unless move_good
       end
 
       #place the move on the board
-      make_move(board,move,player)
+      make_move(move,player)
 
       #check to see who has won the game and see if they want to play again. loop until they give a decent answer
-      if check_win?(board,player)
+      if check_win?(player)
 
         #draw out the board, its nice to see your victory
-        draw_board(board)
+        draw_board()
 
         #reinitilize our variables for the next game
-        board = new_board
+        @board = new_board
         player = 2
         continue = ""
         @num_players = 0
@@ -212,11 +212,11 @@ class TicTacToe
           @num_players = gets.chomp.to_i
         end
       end
-      if check_stalemate?(board,player)
-        draw_board(board)
+      if check_stalemate?(player)
+        draw_board()
 
         #reinitilize our variables for the next game
-        board = new_board
+        @board = new_board
         player = 2
         continue = ""
         @num_players = 0
